abaf7417dee047638b404d27ac214888b1b52573
Vikram Singh <singhvikram143777@gmail.com>
2024-06-11 08:30:26 +0530

    Initial Commit

---
+++ packages/container/config/webpack.common.js
@@ -0,0 +1,24 @@
+const HtmlWebpckPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpckPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello Container</h1>
+      <MarketingApp />
+    </>
+  );
+};
+export default App;
